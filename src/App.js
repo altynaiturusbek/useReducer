f1ed7c1508c0f862775af18ccf1be5f690901101
Altynai <altinayturusbek@gmail.com>
2022-05-11 12:00:25 +0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+ import React, { useReducer } from 'react'
+// import React {useState} from 'react';
+import './App.css';
+
+const initialState=0;
+
+function reducer(state,action) {
+  switch (action.type){
+    case"increment":
+    return state +1;
+    case"decrement":
+    return state -1;
+    default:
+      throw new Error();
+
+
+  }
+}
+
+function App() {
+  // const [counter,setCounter] =useState(0);
+  const[state,dispatch]=useReducer(reducer,initialState);
+
+  // const increment =()=>{
+  //   setCounter(counter+1);
+  // };
+
+  // const decrement=()=>{
+  //   setCounter(counter-1)
+  // }
+
+  return (
+    <div className="App border">
+      <h1>useReducer</h1>
+      <h2>{state}</h2>
+      <button onClick={()=>dispatch({type:'increment'})}>Increment</button>
+      <button onClick={()=>dispatch({type:'decrement'})}>Decrement</button>
+      
+    </div>
+  );
+}
+
+export default App;
